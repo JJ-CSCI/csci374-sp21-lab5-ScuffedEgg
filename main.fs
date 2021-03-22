70474f9478969e1ada176da94e8930bb44b611e8
ScuffedEgg <hlinwu92@gmail.com>
2021-03-22 22:52:24 +0000

    Module 5 - Lab

--- main.fs
+++ main.fs
@@ -3,9 +3,15 @@ module Assignment
 type Tree =
     | Node of Tree * Tree
     | Leaf of int
+    // let tree = Node(Node(Leaf(1), Node(Leaf(2), Leaf(3))), Node(Node(Leaf(4), Leaf(5)), Leaf(6)))
+    // let tree1 = Node(Leaf(2), Leaf(3))
+
 
 let rec prod (t:Tree) :int =
-    0
+ let (a,b)
+    match t with
+    // leaf only has int value
+    |Leaf i
 
 let rec map (f:int->int) (t:Tree) :Tree =
     t
